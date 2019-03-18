a80ec5a15007c13976d0d271b93e226eedbcd40d
Barbara ROMEO <bromeo@neogeo.fr>
2019-03-18 17:18:12 +0100

    Retours usagers, header, datatype catégories

--- ckanext/idgotheme/fanstatic/js/main.js
+++ ckanext/idgotheme/fanstatic/js/main.js
@@ -19,11 +19,20 @@ $(function () {
 
 // Update resource size in resource list (ckan: dataset)
 $('#dataset-resources .resource-list .resource-item [name="size"]').each(function(){
-	var size_bytes = $(this).attr('data-size');
-    $(this).children('span[name="size-value"]').html(toOctetString(size_bytes));
+  var size_bytes = $(this).attr('data-size');
+  $(this).children('span[name="size-value"]').html(toOctetString(size_bytes));
 });
 
 // Update resource size in additional info (ckanext-scheming)
 var resource_size = $('table tr[name="size"] td').html();
 $('table tr[name="size"] td').html(toOctetString(resource_size));
 
+// Modal resource download
+$(document).on("click", ".open-download-modal", function () {
+  var resourceID = $(this).data('id');
+  var resourceName = $(this).data('name');
+  var resourceURL = $(this).data('url');
+  // TODO: AJAX request to get modal text -> $("#download-modal-res-list .modal-custom-content")
+  $("#download-modal-res-list .resource-name").html(resourceName);
+  $("#download-modal-res-list .download-button-resource").attr("href", resourceURL);
+});
