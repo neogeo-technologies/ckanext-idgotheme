3c38fa721b3d4c4c60ed9afb9704c0fc011799bb
Barbara ROMEO <bromeo@neogeo.fr>
2019-05-07 08:52:42 +0200

    Tickets #6790, #6791, #6792, #6793

--- ckanext/idgotheme/fanstatic/js/main.js
+++ ckanext/idgotheme/fanstatic/js/main.js
@@ -29,14 +29,42 @@ $('table tr[name="size"] td').html(toOctetString(resource_size));
 
 // Modal resource download
 $(document).on("click", ".open-download-modal", function () {
-  var datasetName = $(this).data('dataset');
-  var resourceID = $(this).data('id');
-  var resourceName = $(this).data('name');
-  var resourceURL = $(this).data('url');
-  // TODO: AJAX request to get modal text -> $("#download-modal-res-list .modal-custom-content")
-  $("#download-modal-res-list .resource-name").html(resourceName);
+  var datasetName = $(this).data('datasetname');
+  var datasetTitle = $(this).data('datasettitle');
+  var datasetCreationDate = $(this).data('datasetcreationdate');
+  var datasetPublicationDate = $(this).data('datasetpublicationdate');
+  var datasetModificationDate = $(this).data('datasetmodificationdate');
+  var datasetLicense = $(this).data('datasetlicense');
+  var resourceTitle = $(this).data('resourcetitle');
+  var resourceURL = $(this).data('resourceurl');
+  var resourceFormat = $(this).data('resourceformat');
+  var resourceCRS = $(this).data('resourcecrs');
+
+  // Download button URL
   $("#download-modal-res-list .download-button-resource").attr("href", resourceURL);
 
-  var modalCustomContent = $("#download-modal-res-list .modal-custom-content").html().replace('{{URL_DATASET}}',`dataset/${datasetName}`);
+  // Replace variables
+  var regexDatasetURL = new RegExp('<a href="{{URL_DATASET}}">', "g");
+  var modalCustomContent = $("#download-modal-res-list .modal-custom-content").html()
+    .replace(/{{TITLE_DATASET}}/g,`<span class="modal-dataset-title"></span>`)
+    .replace(regexDatasetURL,`<a class="modal-dataset-url"></span>`)
+    .replace(/{{DATE_CREATION}}/g,`<span class="modal-dataset-crea-date"></span>`)
+    .replace(/{{DATE_PUBLICATION}}/g,`<span class="modal-dataset-publi-date"></span>`)
+    .replace(/{{DATE_EDITION}}/g,`<span class="modal-dataset-modif-date"></span>`)
+    .replace(/{{LICENSE_DATASET}}/g,`<span class="modal-dataset-license"></span>`)
+    .replace(/{{TITLE_RESOURCE}}/g,`<span class="modal-res-title"></span>`)
+    .replace(/{{FORMAT_RESOURCE}}/g,`<span class="modal-res-format"></span>`)
+    .replace(/{{CRS_RESOURCE}}/g,`<span class="modal-res-crs"></span>`);
+
   $("#download-modal-res-list .modal-custom-content").html(modalCustomContent);
+
+  $("#download-modal-res-list .modal-custom-content .modal-dataset-title").html(datasetTitle);
+  $("#download-modal-res-list .modal-custom-content .modal-dataset-url").attr("href", `/dataset/${datasetName}`);
+  $("#download-modal-res-list .modal-custom-content .modal-dataset-crea-date").html(datasetCreationDate);
+  $("#download-modal-res-list .modal-custom-content .modal-dataset-publi-date").html(datasetPublicationDate);
+  $("#download-modal-res-list .modal-custom-content .modal-dataset-modif-date").html(datasetModificationDate);
+  $("#download-modal-res-list .modal-custom-content .modal-dataset-license").html(datasetLicense);
+  $("#download-modal-res-list .modal-custom-content .modal-res-title").html(resourceTitle);
+  $("#download-modal-res-list .modal-custom-content .modal-res-format").html(resourceFormat);
+  $("#download-modal-res-list .modal-custom-content .modal-res-crs").html(resourceCRS);
 });
