f73cec776a8a8ffdea041ec4c8a1c337a00bd646
Co Flc <akwalith@gmail.com>
2023-04-13 09:08:12 +0200

    Update validation.py

--- ckanext/idgotheme/validation.py
+++ ckanext/idgotheme/validation.py
@@ -49,9 +49,7 @@ def generic_date_validator(metadata_key, key, data, errors, context):
         else:
             try:
                 date = datetime.datetime.strptime(value, '%Y-%m-%d')
-            except (TypeError, ValueError):
-                raise Invalid(_('Date format incorrect'))
-            except AttributeError:
+            except (TypeError, ValueError, AttributeError):
                 raise Invalid(_('Date format incorrect'))
 
         data[(metadata_key, )] = date.isoformat()
